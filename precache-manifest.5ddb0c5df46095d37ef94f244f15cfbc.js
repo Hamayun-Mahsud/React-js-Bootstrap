529b883ac4b005eb059e1c7b3fe5e0fbf47ceeb0
Hamayun <hr276654@gmail.com>
2020-08-25 16:13:31 +0500

    Updates

---
+++ precache-manifest.5ddb0c5df46095d37ef94f244f15cfbc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17561fba83da08b9ac851942d978567f",
+    "url": "/React-js-Bootstrap/index.html"
+  },
+  {
+    "revision": "f48f43bcc1e777411380",
+    "url": "/React-js-Bootstrap/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "380be186cab06eeb7052",
+    "url": "/React-js-Bootstrap/static/css/main.cf0c1c30.chunk.css"
+  },
+  {
+    "revision": "f48f43bcc1e777411380",
+    "url": "/React-js-Bootstrap/static/js/2.30834304.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-js-Bootstrap/static/js/2.30834304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380be186cab06eeb7052",
+    "url": "/React-js-Bootstrap/static/js/main.2fb59c8d.chunk.js"
+  },
+  {
+    "revision": "8407bd792b53ea6f3144",
+    "url": "/React-js-Bootstrap/static/js/runtime-main.b58fe4a8.js"
+  },
+  {
+    "revision": "675c53b79e8a978e3c72c17ff6bba2cc",
+    "url": "/React-js-Bootstrap/static/media/1.675c53b7.png"
+  },
+  {
+    "revision": "bf167e8251f9cdf209296a948e9752ca",
+    "url": "/React-js-Bootstrap/static/media/2.bf167e82.png"
+  },
+  {
+    "revision": "58e134e07a266f5c0c60067ac8e57d1f",
+    "url": "/React-js-Bootstrap/static/media/Web development.58e134e0.jpeg"
+  },
+  {
+    "revision": "ca70257e0634582d4e07ea0edff6c10f",
+    "url": "/React-js-Bootstrap/static/media/coding.ca70257e.jpeg"
+  },
+  {
+    "revision": "e14c4fe13fe14cd522844457a7ef0ce2",
+    "url": "/React-js-Bootstrap/static/media/office.e14c4fe1.jpg"
+  },
+  {
+    "revision": "9314f0dbf4a5870b23b76d1a4de1e94b",
+    "url": "/React-js-Bootstrap/static/media/programming.9314f0db.jpg"
+  },
+  {
+    "revision": "7a3784d0cc2b5077a2a226652c7085a0",
+    "url": "/React-js-Bootstrap/static/media/software.7a3784d0.webp"
+  },
+  {
+    "revision": "41b6d1603ef89b59ebec50b0af5229ac",
+    "url": "/React-js-Bootstrap/static/media/web.41b6d160.jpg"
+  }
+]);
